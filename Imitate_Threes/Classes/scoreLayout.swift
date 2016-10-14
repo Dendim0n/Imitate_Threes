ab11fde289c47917f327cd926066109f94ff1834
Dendim0n <mspro5@qq.com>
2016-10-14 17:15:46 +0800

    initial work of score -> UICollectionView

---
+++ Imitate_Threes/Classes/scoreLayout.swift
@@ -0,0 +1,13 @@
+//
+//  scoreLayout.swift
+//  Imitate_Threes
+//
+//  Created by 任岐鸣 on 2016/10/14.
+//  Copyright © 2016年 Ned. All rights reserved.
+//
+
+import UIKit
+
+class scoreLayout: UICollectionViewFlowLayout {
+
+}
--- Imitate_Threes/Scores.swift
+++ Imitate_Threes/Scores.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class Scores: UIViewController {
+class Scores: UIViewController,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource {
+    
+    var collectionView:UICollectionView?
+    var collectionLayout:UICollectionViewFlowLayout {
+        get {
+            let layout = UICollectionViewFlowLayout.init()
+            layout.scrollDirection = .horizontal
+            layout.itemSize = CGSize.init(width: view.frame.size.width * 0.7, height: view.frame.size.height * 0.8)
+            return layout
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +33,30 @@ class Scores: UIViewController {
     
     func setView() {
         
+        collectionView = UICollectionView.init(frame: CGRect.zero, collectionViewLayout: collectionLayout)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        view.addSubview(collectionView!)
+        collectionView?.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .gray
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
 
 }
